4c50dd4b34f6a6b2b65c3fb96a191a9aa01f2d4f
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-27 23:28:39 +0100

    Solutions for day 9

--- Day09/part1.py
+++ Day09/part1.py
@@ -1,9 +1,15 @@
 import time
+import re
 
 start_time = time.time()
 
 with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    line = [line.rstrip('\n') for line in f.readlines()][0]
 
-print(len(lines))
-print(time.time() - start_time)
+n_garbage = 0
+for garbage in re.finditer('<([^!>]|!.)*>', line):
+    garbage = garbage.group()[1:-1]
+    n_garbage += len([s for s in re.findall('([^!]|!.)', garbage) if s[0] != '!'])
+
+
+print(n_garbage)
--- Day09/part2.py
+++ Day09/part2.py
@@ -1,9 +1,47 @@
 import time
+import re
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
-print(time.time() - start_time)
+
+def dfs_group(group, depth):
+    score = depth
+    group = group[1:-1].rstrip(',')
+    if group == '':
+        return score
+
+    aantal = 0
+    for index, c in enumerate(group):
+        if c == '{':
+            aantal += 1
+            if aantal == 0:
+                start_index = index
+        elif c == '}':
+            aantal -= 1
+
+        if aantal == 0:
+            score += dfs_group(group[start_index:index + 1], depth + 1)
+    return score
+
+
+
+
+cleaned = re.sub('<([^!>]|!.)*>', '<>', lines[0])
+
+
+
+score = 0
+aantal = 0
+
+for char in cleaned:
+    if char == '{':
+        aantal += 1
+        score += aantal
+
+    elif char == '}':
+        aantal -= 1
+
+print(score)
