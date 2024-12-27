16ece91530fc6489967a73889c79158d390baf82
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-27 22:17:48 +0100

    Solutions for day 8

--- Day08/part1.py
+++ Day08/part1.py
@@ -1,9 +1,32 @@
 import time
+from collections import defaultdict
+import re
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+registers = defaultdict(int)
+
+test = {'<': lambda a, b: a < b,
+        '<=': lambda a, b: a <= b,
+        '>': lambda a, b: a > b,
+        '>=': lambda a, b: a >= b,
+        '==': lambda a, b: a == b,
+        '!=': lambda a, b: a != b,
+        }
+
+funcs = {'inc': lambda a, b: a + b, 'dec': lambda a, b: a - b}
+
+for line in lines:
+    m = re.match('([^ ]+) (inc|dec) ([^ ]+) if ([^ ]+) ([^ ]+) ([^ ]+)', line)
+
+    r1, f, n, c1, cf, c2 = m.groups()
+
+    if test[cf](registers[c1], int(c2)):
+        registers[r1] = funcs[f](registers[r1], int(n))
+
+print(max(registers.values()))
+
 print(time.time() - start_time)
--- Day08/part2.py
+++ Day08/part2.py
@@ -1,9 +1,37 @@
+import math
 import time
+from collections import defaultdict
+import re
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+registers = defaultdict(int)
+
+test = {'<': lambda a, b: a < b,
+        '<=': lambda a, b: a <= b,
+        '>': lambda a, b: a > b,
+        '>=': lambda a, b: a >= b,
+        '==': lambda a, b: a == b,
+        '!=': lambda a, b: a != b,
+        }
+
+funcs = {'inc': lambda a, b: a + b, 'dec': lambda a, b: a - b}
+higest_ever = -math.inf
+
+for line in lines:
+    m = re.match('([^ ]+) (inc|dec) ([^ ]+) if ([^ ]+) ([^ ]+) ([^ ]+)', line)
+
+    r1, f, n, c1, cf, c2 = m.groups()
+
+    if test[cf](registers[c1], int(c2)):
+        new_value = funcs[f](registers[r1], int(n))
+        registers[r1] = new_value
+        if new_value>higest_ever:
+            higest_ever = new_value
+
+print(higest_ever)
+
 print(time.time() - start_time)
