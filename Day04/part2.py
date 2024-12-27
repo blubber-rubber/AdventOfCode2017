2f012e68acd092df5273fc6006920f42a22ccb5f
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-27 21:09:23 +0100

    Solutions for day 4

--- Day04/part2.py
+++ Day04/part2.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import time
 
 start_time = time.time()
@@ -5,5 +7,10 @@ start_time = time.time()
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+n_passphrases = 0
+for line in lines:
+    if all(c == 1 for c in Counter(["".join(sorted(x for x in word)) for word in line.split(' ')]).values()):
+        n_passphrases += 1
+
+print(n_passphrases)
 print(time.time() - start_time)
