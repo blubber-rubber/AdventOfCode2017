5ced0c015cbf996b0fa84c003605a0597835b39a
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-27 21:02:07 +0100

    Solutions for day 2

--- Day02/part1.py
+++ Day02/part1.py
@@ -5,5 +5,9 @@ start_time = time.time()
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+som = 0
+for line in lines:
+    numbers = [int(x) for x in line.split('	')]
+    som += max(numbers) - min(numbers)
+print(som)
 print(time.time() - start_time)
--- Day02/part2.py
+++ Day02/part2.py
@@ -1,9 +1,18 @@
 import time
+from itertools import combinations
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+som = 0
+for line in lines:
+    numbers = [int(x) for x in line.split('	')]
+    for a, b in combinations(numbers, 2):
+        if a % b == 0:
+            som += a // b
+        elif b % a == 0:
+            som += b // a
+print(som)
 print(time.time() - start_time)
