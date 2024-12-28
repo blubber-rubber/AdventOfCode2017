a4ade10b0372cc66b9f2ee45a3248473cb2dd544
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-28 01:50:41 +0100

    Solutions for day 13

--- Day13/part1.py
+++ Day13/part1.py
@@ -5,5 +5,10 @@ start_time = time.time()
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+score = 0
+for line in lines:
+    depth, rng = (int(x) for x in line.split(': '))
+    score += depth * rng * (depth % (2*(rng-1)) == 0)
+
+print(score)
 print(time.time() - start_time)
--- Day13/part2.py
+++ Day13/part2.py
@@ -5,5 +5,10 @@ start_time = time.time()
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+info = [tuple(int(x) for x in line.split(': ')) for line in lines]
+
+wait = 0
+while not all((wait + depth) % (2 * (rng - 1)) for depth, rng in info):
+    wait += 1
+print(wait)
 print(time.time() - start_time)
