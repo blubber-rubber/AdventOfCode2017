ce6d4de8867a345d06a471a48e64bd6fb88053e8
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-28 01:19:27 +0100

    Solutions for day 10

--- Day10/part1.py
+++ Day10/part1.py
@@ -3,7 +3,20 @@ import time
 start_time = time.time()
 
 with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    lijst = [int(x) for x in f.readlines()[0].rstrip('\n').split(',')]
 
-print(len(lines))
+# lijst = [3, 4, 1, 5]
+
+circle = list(range(256))
+cp = 0
+skip_size = 0
+
+for x in lijst:
+    circle = circle[cp:] + circle[:cp]
+    circle[:x] = circle[:x][::-1]
+    circle = circle[-cp:] + circle[:(len(circle)-cp)%len(circle)]
+    cp = (cp+x + skip_size) % len(circle)
+    skip_size += 1
+
+print(circle[0]*circle[1])
 print(time.time() - start_time)
--- Day10/part2.py
+++ Day10/part2.py
@@ -1,9 +1,35 @@
 import time
+from functools import reduce
 
 start_time = time.time()
 
 with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    lijst = [ord(x) for x in f.readlines()[0].rstrip('\n')]
 
-print(len(lines))
+lijst += [17, 31, 73, 47, 23]
+
+circle = list(range(256))
+cp = 0
+skip_size = 0
+
+for _ in range(64):
+
+    for x in lijst:
+        circle = circle[cp:] + circle[:cp]
+        circle[:x] = circle[:x][::-1]
+        circle = circle[-cp:] + circle[:(len(circle) - cp) % len(circle)]
+        cp = (cp + x + skip_size) % len(circle)
+        skip_size += 1
+
+dense_hash = []
+
+for i in range(16):
+    dense_hash.append(reduce(lambda a, b: a ^ b, circle[16 * i:16 * (i + 1)]))
+
+hash = ''
+for c in dense_hash:
+    hash += f'{hex(c)[2:]:>02}'
+
+print(hash)
+
 print(time.time() - start_time)
