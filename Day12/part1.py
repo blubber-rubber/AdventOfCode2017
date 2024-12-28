55578ce6640a42b205ea3462d3f67ecacf2fcb08
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-28 01:32:17 +0100

    Solutions for day 12

--- Day12/part1.py
+++ Day12/part1.py
@@ -1,9 +1,29 @@
 import time
+from collections import defaultdict
 
 start_time = time.time()
+
+graph = defaultdict(set)
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+for line in lines:
+    a, lijst = line.split(' <-> ')
+    for x in lijst.split(', '):
+        graph[a].add(x)
+        graph[x].add(a)
+
+visited = {'0'}
+stack = ['0']
+
+while stack:
+    node = stack.pop()
+    for other in graph[node]:
+        if other not in visited:
+            visited.add(other)
+            stack.append(other)
+
+print(len(visited))
+
 print(time.time() - start_time)
--- Day12/part2.py
+++ Day12/part2.py
@@ -1,9 +1,36 @@
 import time
+from collections import defaultdict
 
 start_time = time.time()
+
+graph = defaultdict(set)
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+for line in lines:
+    a, lijst = line.split(' <-> ')
+    for x in lijst.split(', '):
+        graph[a].add(x)
+        graph[x].add(a)
+
+index = 1
+visited = {}
+
+for key in graph:
+    if key not in visited:
+        visited[key] = index
+        stack = [key]
+
+        while stack:
+            node = stack.pop()
+            for other in graph[node]:
+                if other not in visited:
+                    visited[other] = index
+                    stack.append(other)
+
+        index += 1
+
+print(max(visited.values()))
+
 print(time.time() - start_time)
