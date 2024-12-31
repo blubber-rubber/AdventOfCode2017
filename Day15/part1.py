205292d749d71cff9070c57f5b90e071f1f85e68
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-31 11:11:41 +0100

    Solutions for day 15

--- Day15/part1.py
+++ Day15/part1.py
@@ -1,9 +1,28 @@
 import time
+from tqdm import tqdm
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+VA = 618
+VB = 814
+
+MA = 16807
+MB = 48271
+
+M = 2147483647
+m = 2 ** 16
+
+count = 0
+
+for _ in tqdm(range(40_000_000)):
+    VA = (VA * MA) % M
+    VB = (VB * MB) % M
+    cA = VA % m
+    cB = VB % m
+    count += (cA == cB)
+
+print(count)
 print(time.time() - start_time)
--- Day15/part2.py
+++ Day15/part2.py
@@ -1,9 +1,40 @@
 import time
+from tqdm import tqdm
 
 start_time = time.time()
 
-with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+MOD = 2147483647
+m = 2 ** 16
 
-print(len(lines))
+
+class Generator:
+
+    def __init__(self, start_value, multiplier, condition):
+        self.V = start_value
+        self.M = multiplier
+        self.C = condition
+
+    def __next__(self):
+        self.V = (self.V * self.M) % MOD
+        while self.V % self.C != 0:
+            self.V = (self.V * self.M) % MOD
+        return self.V % m
+
+
+VA = 618
+VB = 814
+
+MA = 16807
+MB = 48271
+
+GA = Generator(VA, MA, 4)
+GB = Generator(VB, MB, 8)
+count = 0
+
+for _ in tqdm(range(5_000_000)):
+    cA = next(GA)
+    cB = next(GB)
+    count += (cA == cB)
+
+print(count)
 print(time.time() - start_time)
