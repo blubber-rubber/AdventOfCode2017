2971e66d7859f3bc8ad8b62c6a9e1f76b7d69c0d
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-31 14:03:48 +0100

    Solutions for day 18

--- Day18/part1.py
+++ Day18/part1.py
@@ -1,9 +1,63 @@
 import time
+from collections import defaultdict
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+REGISTERS = defaultdict(int)
+OUTPUTS = []
+RECOVERED = []
+
+
+def get_value(v):
+    if v in REGISTERS:
+        return REGISTERS[v]
+    return int(v)
+
+
+def snd(x):
+    OUTPUTS.append(get_value(x))
+
+
+def set(x, y):
+    REGISTERS[x] = get_value(y)
+
+
+def add(x, y):
+    REGISTERS[x] += get_value(y)
+
+
+def mul(x, y):
+    REGISTERS[x] *= get_value(y)
+
+
+def mod(x, y):
+    REGISTERS[x] %= get_value(y)
+
+
+def rcv(x):
+    if get_value(x) != 0:
+        RECOVERED.append(OUTPUTS[-1])
+
+
+def jgz(x, y):
+    if get_value(x) > 0:
+        return get_value(y)
+
+
+pointer = 0
+
+funcs = {'snd': snd, 'set': set, 'add': add, 'mul': mul, 'jgz': jgz, 'mod': mod, 'rcv': rcv}
+
+while pointer < len(lines) and len(RECOVERED) == 0:
+    f, *a = lines[pointer].split(' ')
+    r = funcs[f](*a)
+    if r:
+        pointer += r
+    else:
+        pointer += 1
+
+print(RECOVERED[-1])
 print(time.time() - start_time)
--- Day18/part2.py
+++ Day18/part2.py
@@ -1,9 +1,90 @@
 import time
+from collections import defaultdict
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+
+class Program:
+
+    def __init__(self, id):
+        self.REGISTERS = defaultdict(int)
+        self.REGISTERS['p'] = id
+        self.OUTPUTS = []
+        self.pointer = 0
+        self.pair = None
+        self.heard = 0
+        self.id = id
+        self.funcs = {'snd': self.snd, 'set': self.set, 'add': self.add, 'mul': self.mul, 'jgz': self.jgz,
+                      'mod': self.mod, 'rcv': self.rcv}
+
+        self.waiting = False
+
+    def get_value(self, v):
+        if v in self.REGISTERS:
+            return self.REGISTERS[v]
+        return int(v)
+
+    def snd(self, x):
+        self.OUTPUTS.append(self.get_value(x))
+
+    def set(self, x, y):
+        self.REGISTERS[x] = self.get_value(y)
+
+    def add(self, x, y):
+        self.REGISTERS[x] += self.get_value(y)
+
+    def mul(self, x, y):
+        self.REGISTERS[x] *= self.get_value(y)
+
+    def mod(self, x, y):
+        self.REGISTERS[x] %= self.get_value(y)
+
+    def rcv(self, x):
+        if self.waiting:
+            # print(self, self.pair)
+            raise Exception
+
+        if len(self.pair.OUTPUTS) < self.heard + 1:
+            self.waiting = True
+            self.pair.run()
+            self.waiting = False
+        self.REGISTERS[x] = self.pair.OUTPUTS[self.heard]
+        self.heard += 1
+
+    def jgz(self, x, y):
+        if self.get_value(x) > 0:
+            return self.get_value(y)
+
+    def run_to_sound(self, desired_length):
+        while len(self.OUTPUTS) < desired_length:
+            f, *a = lines[self.pointer].split(' ')
+            r = self.funcs[f](*a)
+            if r:
+                self.pointer += r
+            else:
+                self.pointer += 1
+
+    def run(self):
+        self.run_to_sound(len(self.OUTPUTS) + 1)
+
+    def __str__(self):
+        return f'Program {self.id} emitted {len(self.OUTPUTS)} sounds'
+
+
+p0 = Program(0)
+p1 = Program(1)
+p0.pair = p1
+p1.pair = p0
+
+while True:
+    try:
+        p0.run()
+    except Exception:
+        break
+
+
+print(len(p1.OUTPUTS))
 print(time.time() - start_time)
