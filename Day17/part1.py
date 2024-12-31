9c7f6d9d99b4e1f15efc017de141bae59f14051a
rubenv-locus <118970190+rubenv-locus@users.noreply.github.com>
2024-12-31 13:23:35 +0100

    Solutions for day 17

--- Day17/part1.py
+++ Day17/part1.py
@@ -3,7 +3,14 @@ import time
 start_time = time.time()
 
 with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    step_size = int(f.readline())
 
-print(len(lines))
+circular_buffer = [0]
+cp = 0
+for value in range(1, 2018):
+    cp = (cp + step_size) % len(circular_buffer)
+    cp += 1
+    circular_buffer.insert(cp, value)
+
+print(circular_buffer[(cp + 1) % len(circular_buffer)])
 print(time.time() - start_time)
--- Day17/part2.py
+++ Day17/part2.py
@@ -1,9 +1,20 @@
 import time
+from tqdm import tqdm
 
 start_time = time.time()
 
 with open('input.txt') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    step_size = int(f.readline())
 
-print(len(lines))
-print(time.time() - start_time)
+buffer_length = 1
+cp = 0
+after_zero = None
+for value in tqdm(range(1, 50_000_000 + 1)):
+    cp = (cp + step_size) % buffer_length
+    if cp == 0:
+        after_zero = value
+
+    cp += 1
+    buffer_length += 1
+
+print(after_zero)
